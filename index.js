6bc4808d0e3cfba107480cd27db99546d11e3841
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2023-02-19 15:12:36 +0200

    files for intro-section-with-dropdown-navigation-main

---
+++ index.js
@@ -0,0 +1,36 @@
+let dropdownButton = document.querySelectorAll(".btn");
+let dropButton = document.querySelectorAll(".drop-btn");
+let links = document.querySelectorAll("a");
+
+//preventDefault all <a>
+links.forEach(ele => {
+    ele.addEventListener("click", e => e.preventDefault());
+});
+
+dropdownButton.forEach(ele => {
+    ele.addEventListener("click", e => {
+        e.preventDefault();
+        e.currentTarget.classList.toggle("change-color");
+        e.currentTarget.nextElementSibling.classList.toggle("show");
+        e.currentTarget.classList.toggle("active");
+    });
+});
+
+let aside = document.querySelector(".popup-aside");
+let closeIcon = document.querySelector(".close");
+let mobileDevices = document.querySelector(".mobile");
+
+mobileDevices.addEventListener('click', _ => aside.style.left = "0");
+
+closeIcon.addEventListener("click", _ => {
+    aside.style.left = "100%";
+});
+
+let headOne = document.querySelectorAll("h1");
+console.log(headOne[0]);
+console.log(document.body.clientWidth)
+
+if (document.body.clientWidth < 992) {
+    headOne[0].innerHTML = "Make remote work";
+    console.log("work");
+}
